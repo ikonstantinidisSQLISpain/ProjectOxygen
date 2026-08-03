d73c95161c19798e8ff9962e6c81b10c0dcd4beb
mmolinadiaz <mmolinadiaz@sqli.com>
2026-08-03 14:27:25 +0100

    update activity and EER

--- src/flow/pipelines/bronze_app/timesheet_and_activities.py
+++ src/flow/pipelines/bronze_app/timesheet_and_activities.py
@@ -61,12 +61,56 @@ def create_activity_table():
         F.col("type_lib"),
         F.col("activity"),
         F.col("is_project").astype("boolean"),
-        F.col("project_service_line_id").alias("service_line_id"),
-        F.col("project_department_id").alias("department_id"),
         ###############################################3
         F.col("_metadata.file_path").alias("_source_file"),
         F.col("_metadata.file_name").alias("_source_file_name"),
         F.col("_metadata.file_size").alias("_source_file_size"),
         F.col("_metadata.file_modification_time").alias("_source_file_modified_at")
     ).dropDuplicates()
+    return df
+
+
+
+@dp.table(
+    name=f"{CATALOG}.{TARGET_SCHEMA}.activity_service_line",
+    comment=(
+        "Raw activity data, formatted"
+    ),
+    table_properties={
+        "quality":"bronze"
+    }
+)
+def create_activity_service_line_table():
+    raw = spark.read.option("multiLine", True)\
+                        .json(f"/Volumes/{CATALOG}/{READ_SCHEMA}/source/app_timesheet_report_anonymized.json")
+
+    # They are already unpacked so no need to analyze anything, just
+    # select the type_id and service_line
+    df = raw.select(
+        F.col("type_id"),
+        F.col("project_service_line_id").alias("service_line_id")
+    ).dropna(how='any') # Any cause we don't need a row that gives just and Id from another table.
+    return df
+
+
+
+@dp.table(
+    name=f"{CATALOG}.{TARGET_SCHEMA}.activity_department",
+    comment=(
+        "Raw activity data, formatted"
+    ),
+    table_properties={
+        "quality":"bronze"
+    }
+)
+def create_activity_department_table():
+    raw = spark.read.option("multiLine", True)\
+                        .json(f"/Volumes/{CATALOG}/{READ_SCHEMA}/source/app_timesheet_report_anonymized.json")
+
+    # They are already unpacked so no need to analyze anything, just
+    # select the type_id and service_line
+    df = raw.select(
+        F.col("type_id"),
+        F.col("project_department_id").alias("department_id")
+    ).dropna(how='any')
     return df
